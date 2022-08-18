9bf11d85da89e2ff5f7de2607fac7e61754679af
sigaurav-eoss <gaurav@eosstech.com>
2022-08-18 23:14:24 +0530

    FastAPI working now

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.stats import norm
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # Define Model Variables
 # Description based on National Stock Exchange, India
@@ -12,22 +13,31 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+class blackScholes(BaseModel):
+
+    r : float
+    S : int
+    K : int
+    T : float
+    sigma : float
+    type : str
+
 # Implementing black scholes model
 @app.get('/')
 def echo():
     return {'message':'echo'}
 
-@app.get('/price')
-def blackScholes(r:float, S:int, K:int, T:float, sigma:float, type:str):
-    d1 = (np.log(S/K) + (r + sigma**2/2)*T)/(sigma * np.sqrt(T))
-    d2 = d1 - sigma*np.sqrt(T)
-
+@app.post('/price')
+def blackScholes(item:blackScholes):
+    d1 = (np.log(item.S/item.K) + (item.r + item.sigma**2/2)*item.T)/(item.sigma * np.sqrt(item.T))
+    d2 = d1 - item.sigma*np.sqrt(item.T)
+    
     try:
-        if type == 'C':
-            optionPrice = S * norm.cdf(d1, 0, 1) - K*np.exp(-r*T)*norm.cdf(d2, 0, 1)
+        if item.type == 'C':
+            optionPrice = item.S * norm.cdf(d1, 0, 1) - item.K*np.exp(-item.r*item.T)*norm.cdf(d2, 0, 1)
             return optionPrice
-        elif type == 'P':
-            optionPrice = K*np.exp(-r*T)*norm.cdf(-d2, 0, 1) - S * norm.cdf(-d1, 0, 1)
+        elif item.type == 'P':
+            optionPrice = item.K*np.exp(-item.r*item.T)*norm.cdf(-d2, 0, 1) - item.S * norm.cdf(-d1, 0, 1)
             return optionPrice
     except Exception as e:
         print(e)   
